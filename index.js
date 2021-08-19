68f5b7346bf0fa2884956d8f1de57c5e31a920bd
waqarakram93-tech <84813788+waqarakram93-tech@users.noreply.github.com>
2021-08-19 21:00:07 +0200

    add

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV !== "production") {
-    const morgan = await import("morgan");
-    app.use(morgan.default("dev"));
+  const morgan = await import("morgan");
+  app.use(morgan.default("dev"));
 }
 app.use(cors());
 app.use(express.json());
 app.use("/posts", postsRouter);
-
+app.all('*', (req, res) => res.status(404).json({ error: 'Not found' }));
 app.use(errorHandler);
-console.log('app start')
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
